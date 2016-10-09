1e485cd333e6e89902c72855b674aaa6bf388942
kde2950 <ehdjs243@naver.com>
2016-10-09 14:33:24 +0900

    asdf

--- asdf/main.c
+++ asdf/main.c
@@ -1,15 +1,104 @@
-//
-//  main.c
-//  asdf
-//
-//  Created by KimDoEon on 2016. 10. 9..
-//  Copyright © 2016년 KimDoEon. All rights reserved.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+int main() {
+    printf("1\n");
+    printf("2\n");
+    printf("3\n");
+    printf("4\n");
+    printf("5\n");
+    printf("6\n");
+    printf("7\n");
+    printf("8\n");
+    printf("9\n");
+    printf("10\n");
+    printf("11\n");
+    printf("12\n");
+    printf("13\n");
+    printf("14\n");
+    printf("15\n");
+    printf("16\n");
+    printf("17\n");
+    printf("18\n");
+    printf("19\n");
+    printf("20\n");
+    printf("21\n");
+    printf("22\n");
+    printf("23\n");
+    printf("24\n");
+    printf("25\n");
+    printf("26\n");
+    printf("27\n");
+    printf("28\n");
+    printf("29\n");
+    printf("30\n");
+    printf("31\n");
+    printf("32\n");
+    printf("33\n");
+    printf("34\n");
+    printf("35\n");
+    printf("36\n");
+    printf("37\n");
+    printf("38\n");
+    printf("39\n");
+    printf("40\n");
+    printf("41\n");
+    printf("42\n");
+    printf("43\n");
+    printf("44\n");
+    printf("45\n");
+    printf("46\n");
+    printf("47\n");
+    printf("48\n");
+    printf("49\n");
+    printf("50\n");
+    printf("51\n");
+    printf("52\n");
+    printf("53\n");
+    printf("54\n");
+    printf("55\n");
+    printf("56\n");
+    printf("57\n");
+    printf("58\n");
+    printf("59\n");
+    printf("60\n");
+    printf("61\n");
+    printf("62\n");
+    printf("63\n");
+    printf("64\n");
+    printf("65\n");
+    printf("66\n");
+    printf("67\n");
+    printf("68\n");
+    printf("69\n");
+    printf("70\n");
+    printf("71\n");
+    printf("72\n");
+    printf("73\n");
+    printf("74\n");
+    printf("75\n");
+    printf("76\n");
+    printf("77\n");
+    printf("78\n");
+    printf("79\n");
+    printf("80\n");
+    printf("81\n");
+    printf("82\n");
+    printf("83\n");
+    printf("84\n");
+    printf("85\n");
+    printf("86\n");
+    printf("87\n");
+    printf("88\n");
+    printf("89\n");
+    printf("90\n");
+    printf("91\n");
+    printf("92\n");
+    printf("93\n");
+    printf("94\n");
+    printf("95\n");
+    printf("96\n");
+    printf("97\n");
+    printf("98\n");
+    printf("99\n");
+    printf("100\n");
+    }
